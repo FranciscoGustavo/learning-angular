0b0a3b3c27ef1080aa53d1451a31eaea75f1332c
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-08-05 00:11:23 -0500

    👷‍♂️[Add] getHeroe in service

--- comic-app/src/app/components/heroe/heroe.component.ts
+++ comic-app/src/app/components/heroe/heroe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  public heroe: Heroe;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      console.log(params.id);
+      this.heroe = this.heroesService.getHeroe(params.id);
+    });
+  }
 
   ngOnInit(): void {
   }

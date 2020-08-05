fcb54cf8d3a884a0d557afd47eb40b1a61c9ca70
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-08-04 22:47:16 -0500

    👷‍♂️[Init] images and page heroes and about

--- comic-app/src/app/app.module.ts
+++ comic-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { HeroesComponent } from './componentes/heroes/heroes.component';
+import { AboutComponent } from './componentes/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
